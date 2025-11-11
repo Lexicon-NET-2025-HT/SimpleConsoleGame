c16bd678f141e203fbb0a64c9c2435f42b757a9d
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2025-11-11 12:30:50 +0100

    Add project files.

---
+++ SimpleConsoleGame/Map.cs
@@ -0,0 +1,16 @@
+﻿internal class Map
+{
+
+    private Cell[,] _cells;
+    
+    private int height;
+    private int width;
+
+    public Map(int height, int width)
+    {
+        this.height = height;
+        this.width = width;
+
+        _cells = new Cell[height, width];
+    }
+}
