965e413bbc86ca58a8531529a5247fcf0b89fd93
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2025-11-13 13:45:48 +0100

    Start on limitedlist

--- SimpleConsoleGame.LimitedList/LimitedsList.cs
+++ SimpleConsoleGame.LimitedList/LimitedsList.cs
@@ -1,6 +1,37 @@
-﻿namespace SimpleConsoleGame.LimitedList;
+﻿using System.Collections;
 
-public class LimitedsList<T>
+namespace SimpleConsoleGame.LimitedList;
+
+public class LimitedsList<T>  : IEnumerable<T>
 {
+    private List<T> _list;
+    private readonly int _capacity;
+    public int Count => _list.Count;
+    public bool IsFull => _capacity <= Count;
 
+    public LimitedsList(int capacity)
+    {
+        _capacity = Math.Max(capacity, 2);
+        _list = new List<T>(_capacity);
+    }
+
+    public bool Add(T item)
+    {
+        if(IsFull)       return false;
+        _list.Add(item); return true;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (T item in _list)
+        {
+            //....
+            //....
+            //....
+           yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+   
 }
--- SimpleConsoleGame/Program.cs
+++ SimpleConsoleGame/Program.cs
@@ -1,6 +1,27 @@
 ﻿
-var li = new List<int>();
-var li2 = new List<Creature>();
+using SimpleConsoleGame.LimitedList;
+
+var name = "Kalle Anka";
+
+foreach (var item in name)
+{
+    Console.WriteLine(item);
+}
+
+var li2 = new List<int>();
+
+foreach (var item in li2)
+{
+    Console.WriteLine(item);
+}
+
+
+var lm = new LimitedsList<int>(10);
+
+foreach (var item in lm)
+{
+    Console.WriteLine(item);
+}
 
 var game = new Game();
 game.Run();
